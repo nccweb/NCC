54c7683927aeb028a144ca94d1a97f6c18ae1a35
greazedlightning <rajeev.hanabe@gmail.com>
2014-11-06 14:04:54 +0000

    Migrate all files

---
+++ NCC/WebContent/libraries/Newsletter/js/scripts.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function() {
+	$('#sidebar').affix({
+		offset : {
+			top : 230,
+			bottom : 100
+		}
+	});
+	$('#midCol').affix({
+		offset : {
+			top : 230,
+			bottom : 100
+		}
+	});
+});
